555fc40111ae3b06ba7db488049721db1cb82883
peter <niceguy0825@gmail.com>
2022-01-27 21:45:29 +0900

    6.3.2 리스트, 배열 처리

--- src/main/java/org/zerock/controller/SampleController.java
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -1,9 +1,13 @@
 package org.zerock.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.zerock.domain.SampleDTO;
 
 import lombok.extern.log4j.Log4j;
@@ -21,4 +25,36 @@ public class SampleController {
 		//log.info(" "+dto);
         return "ex01";
     }
+	
+	// http://localhost:8080/sample/ex02?name=AAA&age=10 
+	// @RequestParam으로 객체를 형성하지 않고, 개별적으로 값 전달 가능
+	
+	@GetMapping("/ex02")
+    public String ex02(@RequestParam("name") String name,@RequestParam("age") int age) {
+        System.out.println("name: "+ name);
+        System.out.println("age: "+ age);
+ 
+        return "ex02";
+    }
+	
+	// http://localhost:8080/sample/ex02List?ids=111&ids=222&ids=333
+	// 리스트도 동일하게 전달 가능 
+	
+	@GetMapping("/ex02List")
+    public String ex02List(@RequestParam("ids") ArrayList<String> ids) {
+        System.out.println("ids: "+ ids);
+    
+        return "ex02List";
+    }
+	
+	// http://localhost:8080/sample/ex02List?ids=111&ids=222&ids=333
+	// 배열도 동일하게 전달 가능 
+	
+	@GetMapping("/ex02Array")    
+	public String ex02Array(@RequestParam("ids") String[]ids) {
+		System.out.println("array ids: "+ Arrays.toString(ids));
+		
+        return "ex02Array";    
+    }
+	
 }
